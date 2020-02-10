8f8a1ed45d38743c064439c3bab4512aee17a665
Kyle C <kylecilia@gmail.com>
2020-02-09 19:18:56 -0800

    Allows user to select search radius now

--- public/js/index.js
+++ public/js/index.js
@@ -1,19 +1,22 @@
 $(document).ready(function() {
 	// Get references to page elements
-	var $searchText = $('#search-text');
-	var $searchTextAdd = $('#search-text');
-	var $submitBtn = $('#submit');
-
-	var handleAddButton = function(event) {
+	const $searchText = $('#search-text');
+	const $searchTextAdd = $('#search-text');
+	const $submitBtn = $('#submit');
+	const $searchRadius = $('#search-radius');
+	
+	const handleAddButton = function(event) {
 		event.preventDefault();
 
 		let search = $searchText.val().trim();
+		let searchRadius = $searchRadius.val();
+		console.log(`searchRadius:: ${searchRadius}`);
 		if (!search) {
 			alert('You must enter a city or town to search nearby!');
 			return;
 		}
 
-		let url = `/location/${search}`;
+		let url = `/location/${search}&${searchRadius}`;
 
 		console.log('hello');
 		var data_row;
@@ -66,7 +69,6 @@ $(document).ready(function() {
 			alert('You must enter a city or town to search nearby!');
 			return;
 		}
-
 		location.replace(url);
 	};
 
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,35 +1,35 @@
 var db = require('../models');
 module.exports = function(app) {
-app.post("/api/add", function(req, res) {
-	console.log (req.body)
-    db.destination_search.create(req.body)
-      .then(function(dbPost) {
-        res.json(dbPost);
-      });
-  });
-
-app.get("/api/get/:location_name", function(req, res) {
-
-	db.destination_search.findAll({
-		where: {
-			destination_name: req.params.location_name
-		}
-	}).then(function(destination_search) {
-		res.json(destination_search)
-	})
-});
-
-app.put("/api/update/", function(req, res) {
-	console.log("update body")
-	console.log (req.body)
-	db.destination_search.update(req.body,
-		{
-			where: {
-				id: req.body.id
-			}
-		})
-		.then(function(dbPost) {
+	app.post('/api/add', function(req, res) {
+		console.log(req.body);
+		db.destination_search.create(req.body).then(function(dbPost) {
 			res.json(dbPost);
 		});
+	});
+
+	app.get('/api/get/:location_name', function(req, res) {
+		db.destination_search
+			.findAll({
+				where: {
+					destination_name: req.params.location_name
+				}
+			})	
+			.then(function(destination_search) {
+				res.json(destination_search);
+			});
+	});
+
+	app.put('/api/update/', function(req, res) {
+		console.log('update body');
+		console.log(req.body);
+		db.destination_search
+			.update(req.body, {
+				where: {
+					id: req.body.id
+				}
+			})
+			.then(function(dbPost) {
+				res.json(dbPost);
+			});
 	});
 };
